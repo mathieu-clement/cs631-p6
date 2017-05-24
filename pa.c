025946731ef401ad489b2d6617b92b0a8c6d98f1
Mathieu Clement <mclement2@dons.usfca.edu>
2017-05-24 13:09:57 -0700

    Remove unreachable code

--- pa.c
+++ pa.c
@@ -89,30 +89,7 @@ int main(int argc, char* argv[])
         }
     }
 
-    exit(0);
-
-	int fd;
-
-	if ((fd = open("out", O_CREAT | O_WRONLY, 0644)) < 0) {
-			printf("cannot open out\n");
-			exit(1);
-	}
-	
-	pid_t id = fork();
-
-	if (id == 0) {
-			/* we are in the child */
-			/* close stdout in child */
-			close(1);
-			dup(fd);   /* replace stdout in child with "out" */
-			close(fd);
-			execl("/bin/date", "date", NULL);
-	}
-
-	/* we are in the parent */
-	close(fd);
-	id = wait(NULL);
-
     return 0;
+
 }
 
